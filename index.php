59e648fca711e2e0ce40afb3e5c58b6f36169273
Kausinho <kausinhobrunow@gmail.com>
2021-05-21 11:07:50 -0300

    Estrutura da sessão header do site
    Realizada a estrutura da sessão header do site.

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Meta tags-->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="keywords" content="marketing digital,agência,automação de marketing" />
+    <meta name="author" content="Danki Code" />
+    <!--Links do site-->
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <title>Projeto 10</title>
+</head>
+<body>
+    
+    <header>
+        <div class="container">
+            <div class="menu-topo">
+                <div class="logo"></div><!--logo-->
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Serviços</a></li>
+                        <li><a href="">Contato</a></li>
+                        <li class="btn-nav"><a href="">Quero um orçamento</a></li>
+                    </ul>
+                </nav><!--desktop-->
+                <div class="clear"></div><!--clear-->
+            </div><!--mneu-topo-->
+            <div class="chamada-header">
+                <h2>Você vendendo mais<br />com automação web.</h2>
+                <p>Nós criamos soluções de marketing para sua empresa<br />vender mais, da melhor forma.</p>
+                <form>
+                    <input type="email" required="" placeholder="Seu e-mail..." />
+                    <input type="submit" name="acao" value="Quero saber mais!" />
+                </form>
+            </div><!---chamada-header-->
+        </div><!--container-->
+    </header>
+
+    <script src="js/file.js"></script>
+</body>
+</html>
